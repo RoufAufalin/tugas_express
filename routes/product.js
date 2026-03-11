7416bf57a66def29d2bcefa378bfdb350def0e48
Ro'uf Aufalin Al Ghoitsal <roufaufal01@gmail.com>
2026-03-11 15:01:07 +0700

    anjay

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const { authentication } = require("../middleware/auth")
 
+const coba = ""
+
 const controller = require("../controllers/controllers")
 
 router.get("/", controller.getAllProduct)
